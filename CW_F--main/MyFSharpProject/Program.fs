791b37f3d2cff2fe250e3c203cf69b181de89df7
TrumpLook <82106445+TrumpLook@users.noreply.github.com>
2024-10-07 00:56:17 +0300

    Delete CW_F--main directory

--- CW_F--main/MyFSharpProject/Program.fs
+++
@@ -1,8 +0,0 @@
-﻿open System
-open Parser
-open Interpreter
-
-let text = System.IO.File.ReadAllText("sample.x")
-let tokens = Parser.make_token(text |> Seq.toList)
-let parsed = Parser.parse(tokens |> Seq.toList)
-let result = Interpreter.evaluate parsed Map.empty
